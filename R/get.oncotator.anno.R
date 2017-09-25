7e262e463556cbb275c016ef3153ec8d9a5ac15d
LiNk-NY <mramos09@gmail.com>
2017-09-25 17:45:10 -0400

    add example to get.oncotator.anno

--- R/get.oncotator.anno.R
+++ R/get.oncotator.anno.R
@@ -7,9 +7,12 @@ NULL
 #' @description The get.oncotator.anno retrieves annotation from the Oncotator database.
 #' @param x A matrix containing the columns chromosome, start, end, reference_allele and observed_allele.
 #' @examples
-#' \donttest{
-#' get.oncotator.anno(crcCase_input)
-#' }
+#' exCase <- data.frame(chr = rep(10, 3),
+#'     start = c("100894110", "100985376", "101137905"),
+#'     end = c("100894110", "100985376", "101137905"),
+#'     ref_allele = c("T", "C", "G"),
+#'     obs_allele = c("G", "A", "A"))
+#' get.oncotator.anno(exCase)
 #' @export
 get.oncotator.anno = function(x){
   urls = paste0("http://portals.broadinstitute.org/oncotator/mutation/",
